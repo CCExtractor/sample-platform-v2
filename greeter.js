2e384354a2d0a5166586b70c0cd5f06db6cb59bd
zelzhan <elzhan.zeinulla@nu.edu.kz>
2020-06-02 23:22:59 +0600

    revert: removed unnecessar files from the previous project

--- greeter.js
+++
@@ -1,5 +0,0 @@
-function greeter(person) {
-    return "Hello " + person + "!";
-}
-console.log(greeter('Node Hero'));
-console.log("Hello");
--- greeter.ts
+++
@@ -1,6 +0,0 @@
-function greeter(person: string) {
-  return `Hello ${person}!`
-}
-
-console.log(greeter('Node Hero'))
-console.log("Hello")
