6e55a9268945bf785e1a3ffa2e9c2ed23b9b2375
zelzhan <elzhan.zeinulla@nu.edu.kz>
2020-06-05 08:17:48 +0600

    feat(api): created interface for the VM

---
+++ libs/api-interfaces/src/lib/status-enum.ts
@@ -0,0 +1,5 @@
+export enum Status {
+    Finished,
+    Killed,
+    Running
+}
---
+++ libs/api-interfaces/src/lib/vm-interface.ts
@@ -0,0 +1,10 @@
+import { Status } from "./status-enum"
+
+export interface VMInterface {
+    //TODO: Add more methods. Maybe we should parse desc and
+    //        store the info about the machine inside of variables
+    describe(): String;
+    getStatus(): Status;
+    getTotalRunTime(): number  
+}
+  
