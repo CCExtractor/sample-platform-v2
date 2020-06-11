2a68d590611277a625600949124541212950dbba
zelzhan <elzhan.zeinulla@nu.edu.kz>
2020-06-11 20:02:26 +0600

    feat(api): tests for the orchestration machine and additional config.

---
+++ libs/api-implementation/orchestration/src/lib/orchestration.spec.ts
@@ -0,0 +1,24 @@
+import { VMOrchestrationService } from './orchestration.service'
+import Compute from '@google-cloud/compute'
+import { CreateMachineDTO } from './dto/create-machine.dto';
+
+describe('Orchestration service', () => {
+  let vmOrchestrationService: VMOrchestrationService;
+
+  beforeEach(() => {
+    vmOrchestrationService = new VMOrchestrationService();
+  })
+  
+  describe('createMachine', () => {
+    it('should be created', () => {
+      expect(vmOrchestrationService).toBeTruthy();
+    });
+
+    it('should successfully create a machine', async () => {
+      const createMachineDTO = new CreateMachineDTO();
+      createMachineDTO.name = "test";
+      await vmOrchestrationService.createMachine('test');
+      expect(vmOrchestrationService.getNumberOfRunningInstances()).toEqual(1);
+    });
+  });
+});
