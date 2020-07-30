b11857f100a50a5705d117a91fbb5ddecff681ac
zelzhan <elzhan.zeinulla@nu.edu.kz>
2020-07-31 04:30:59 +0600

    feat(sample-platform): create regression component added

--- libs/frontend/regression-test/src/lib/containers/create-regression-test/create-regression-test.component.ts
+++ libs/frontend/regression-test/src/lib/containers/create-regression-test/create-regression-test.component.ts
@@ -1,25 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'frontend-create-regression-test',
   templateUrl: './create-regression-test.component.html',
   styleUrls: ['./create-regression-test.component.scss'],
 })
-export class CreateRegressionTestComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = [];
-  
-  constructor(private http: HttpClient) {
-    this.getData().subscribe((data: any) => {
-      this.dataSource = data
-    })
-    console.log(this.dataSource)
+export class CreateRegressionTest implements OnInit {
+  checkoutForm;
+
+  value = 'Write regression test here';
+
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      command: '',
+    });
   }
 
-  ngOnInit(): void { }
-  
-  getData() {
-    return this.http.get('/api/regression-test')
+  onSubmit(data) {
+    return this.http
+      .post('/api/regression-test', data)
+      .pipe(
+        tap((res: Response) => {
+          this.router.navigate(['/tests/list']);
+          alert("You have created the regression test!")
+        })
+      )
+      .subscribe();
   }
+
+  ngOnInit(): void {}
 }
--- libs/frontend/regression-test/src/lib/routing.module.ts
+++ libs/frontend/regression-test/src/lib/routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateRegressionTestComponent } from './containers/create-regression-test/create-regression-test.component';
+import { ListRegressionTestsComponent } from './containers/list-regression-tests/list-regression-tests.component';
+import { CreateRegressionTest } from './containers/create-regression-test/create-regression-test.component';
 
-const routes: Routes = [{ path: '', component: CreateRegressionTestComponent }];
+const routes: Routes = [{ path: 'list', component: ListRegressionTestsComponent }, {path: 'create', component: CreateRegressionTest}];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
