a6d25525dccc41d95d478ecfb58912cfbceb72c4
zelzhan <elzhan.zeinulla@nu.edu.kz>
2020-07-30 23:30:09 +0600

    feat(api): added the DTO, POST and GET for controller and service for creation of regression test

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { VMOrchestrationModule } from '../../../../libs/api-implementation/orchestration/src';
+import { ApiImplementationRegressionTestsModule } from '../../../../libs/api-implementation/regression-tests/src'
+import { ApiImplementationDatabaseModule } from '../../../../libs/api-implementation/database/src'
 
 @Module({
-  imports: [VMOrchestrationModule],
+  imports: [VMOrchestrationModule, ApiImplementationRegressionTestsModule, ApiImplementationDatabaseModule],
   controllers: [AppController],
   providers: [],
 })
